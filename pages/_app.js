1e724b796515b6bf6796038e45dbe5edc56d1aa7
Joshua Wood <josh@joshuawood.net>
2021-03-22 11:56:15 -0700

    Get SSR source maps working w/ Honeybadger

--- pages/_app.js
+++ pages/_app.js
@@ -3,15 +3,36 @@ import App from 'next/app'
 import Honeybadger from '@honeybadger-io/js'
 
 // https://docs.honeybadger.io/lib/javascript/reference/configuration.html
-Honeybadger.configure({
+const sharedHoneybadgerConfig = {
   apiKey: process.env.HONEYBADGER_API_KEY,
   revision: process.env.HONEYBADGER_REVISION,
   environment: process.env.NODE_ENV,
-  projectRoot: 'webpack://_N_E/./',
 
   // Uncomment to report errors in development:
   reportData: true,
-})
+}
+
+if (typeof window === 'undefined') {
+  // Node config
+  const projectRoot = process.cwd()
+  Honeybadger.configure({
+    ...sharedHoneybadgerConfig,
+    projectRoot: 'webpack:///./',
+  }).beforeNotify((notice) => {
+    notice.backtrace.forEach((line) => {
+      if (line.file) {
+        line.file = line.file.replace(`${ projectRoot }/.next/server/`, 'http://localhost:3000/_next/')
+      }
+      return line
+    })
+  })
+} else {
+  // Browser config
+  Honeybadger.configure({
+    ...sharedHoneybadgerConfig,
+    projectRoot: 'webpack://_N_E/./',
+  })
+}
 
 // This is handy for testing; remove it in production.
 if (typeof window !== 'undefined') {
