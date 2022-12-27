dc58d6753b3040ab288d1eb16f4ec1f1d8bcd7c4
Bethany Berkowitz <bethanyb@unitworkers.com>
2022-12-27 11:15:02 -0500

    Update for latest nextjs version

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,3 @@
-import React from 'react'
-import App from 'next/app'
 import Honeybadger from '@honeybadger-io/js'
 
 // https://docs.honeybadger.io/lib/javascript/reference/configuration.html
@@ -7,10 +5,11 @@ Honeybadger.configure({
   apiKey: process.env.HONEYBADGER_API_KEY,
   revision: process.env.HONEYBADGER_REVISION,
   environment: process.env.NODE_ENV,
+  // TODO: DIFFERENT FOR SERVER-SIDE?
   projectRoot: 'webpack://_N_E/./',
 
   // Uncomment to report errors in development:
-  reportData: true,
+  // reportData: true,
 })
 
 // This is handy for testing; remove it in production.
@@ -18,16 +17,10 @@ if (typeof window !== 'undefined') {
   window.Honeybadger = Honeybadger
 }
 
-class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props
-
-    // Workaround for https://github.com/zeit/next.js/issues/8592
-    const { err } = this.props
-    const modifiedPageProps = { ...pageProps, err }
-
-    return <Component {...modifiedPageProps} />
-  }
+export default function MyApp({ Component, pageProps, err }) {
+  // Uncomment the workaround below if using Next.js version prior to 12.2.1
+  // getInitialProps is not called in case of https://github.com/zeit/next.js/issues/8592
+  //
+  // pageProps.err = err
+  return <Component {...pageProps} />
 }
-
-export default MyApp
